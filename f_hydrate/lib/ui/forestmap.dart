9229cfba9b9910e875a75e21f32e18685eba9a8e
jowie007 <90611576+jowie007@users.noreply.github.com>
2021-11-19 16:33:42 +0100

    changed desiredaccuracy

--- f_hydrate/lib/ui/forestmap.dart
+++ f_hydrate/lib/ui/forestmap.dart
@@ -84,7 +84,7 @@ class _ForestMapState extends State<ForestMap> {
 
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
-    Position position = await Geolocator.getCurrentPosition();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
     return LatLng(position.latitude, position.longitude);
   }
 
